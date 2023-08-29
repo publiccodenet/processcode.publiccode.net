47e132931fe80c2c93e322d1de6e7462e62c7b3e
mcarlson <mcarlson@users.noreply.github.com>
2023-08-29 02:34:33 +0000

    deploy: 192d7b722d192fa3aa25e8364a6e04e2ccc2dd05

--- process.py
+++ process.py
@@ -9,6 +9,17 @@ new_filename = 'index.html'
 
 os.rename(old_filename, new_filename)
 
+# Rename breakpoint files
+old_filename = '1au46-getting-oriented-all-breakpoints.html'
+new_filename = '1au46-getting-oriented.html'
+
+os.rename(old_filename, new_filename)
+
+old_filename = '1bu46-capacity-building-u40mobileu41-all-breakpoints.html'
+new_filename = '1bu46-capacity-building.html'
+
+os.rename(old_filename, new_filename)
+
 # do string substitution for each pattern across all HTML files
 directory = '.'
 
@@ -20,6 +31,8 @@ patterns = [
 (r"""  </body>""","""     <script src="collapsible.js"></script>
   </body>"""),
 (r"""00u46-process-codes-landing-page.html""", """index.html"""),
+(r"""1au46-getting-oriented-all-breakpoints.html""", """1au46-getting-oriented.html"""),
+(r"""1bu46-capacity-building-u40mobileu41-all-breakpoints.html""", """1bu46-capacity-building.html"""),
 (r"""the agile development process. <br />You will continue running""", """the agile development process. <br /><br />You will continue running"""),
 (r"""expectations?<br />Throughout this process, you also have the crucial responsibility of ensuring that""", """expectations?<br /><br />Throughout this process, you also have the crucial responsibility of ensuring that"""),
 (r"""and expertise doesn’t disappear with individuals.<br />Your""", """and expertise doesn’t disappear with individuals.<br /><br />Your"""),
@@ -52,7 +65,7 @@ for root, dirs, files in os.walk(directory):
 
 cssdirectory = './css/'
 csspatterns = [
-(r"""@import url\("https://px.animaapp.com/6406baa484a3afe9c63921de.6406baa605cc73851b593804.HKtYnxM.hcp.png"\);""", ''),
+(r"""@import url\("https://px.animaapp.com/6406baa484a3afe9c63921de.6406baa605cc73851b593804.*.hcp.png"\);""", ''),
 ]
 # process css
 for root, dirs, files in os.walk(cssdirectory):
